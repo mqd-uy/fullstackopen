e258212b2f8ac3a8ed652857264cab4b4f047766
Mauricio <mauri1987@gmail.com>
2024-01-01 23:17:28 -0300

    Exercise 2.18

---
+++ part2/countries/src/App.jsx
@@ -0,0 +1,71 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+function App() {
+  const [findCountries, setFindCountries] = useState('')
+  const [countries, setCountries] = useState([])
+  const [allCountries, setAllCountries] = useState(null)
+
+  useEffect(() => {
+    const url = 'https://studies.cs.helsinki.fi/restcountries/api/all'
+    axios
+      .get(url)
+      .then(response => setAllCountries(response.data))
+  }, [])
+
+  const handleFindCountries = event => {
+    const searchValue = event.target.value
+    setFindCountries(searchValue)
+    if (allCountries === null || searchValue === '') {
+      setCountries([])
+    } else {
+      const countriesFiltered = allCountries.filter(countrie =>
+        countrie.name.common.toLowerCase().includes(searchValue.toLowerCase())
+      )
+      console.log('countries filtered', countriesFiltered)
+      setCountries(countriesFiltered)
+    }
+
+  }
+
+  const countriesCount = countries.length
+
+  return (
+    <>
+      <label htmlFor="findCountries">find countries</label>
+      <input id="findCountries" value={findCountries} onChange={handleFindCountries} />
+      {countriesCount === 0
+        ? <p>No matches</p>
+        : countriesCount > 10
+          ? <p>Too many matches, specify another filter</p>
+          : countriesCount > 1
+            ? <CountriesList countries={countries} />
+            : <CountryDetails countrie={countries[0]} />
+      }
+
+    </>
+  )
+}
+
+const CountriesList = ({ countries }) => (
+  <ul>
+    {countries.map(countrie =>
+      <li key={countrie.tld}>{countrie.name.common}</li>)}
+  </ul>
+)
+
+const CountryDetails = ({ countrie }) => {
+
+  return <div>
+    <h2>{countrie.name.common}</h2>
+    <p>capital {countrie.capital[0]}</p>
+    <p>area {countrie.area}</p>
+    <h3>languages:</h3>
+    <ul>
+      {Object.values(countrie.languages).map(language => <li key={language}>{language}</li>)}
+    </ul>
+    <img alt={countrie.flags.alt} src={countrie.flags.png} />
+  </div>
+}
+
+export default App
